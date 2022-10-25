2ba1ae8cdd76401c27e5c17dd57a76146519d626
anikshajol <anikshajol@gmail.com>
2022-10-26 01:11:06 +0600

    firebase and react toastify added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCcIAIH78XczAfL5n2CDuQboD338ods8x8",
+  authDomain: "next-coder-6c79f.firebaseapp.com",
+  projectId: "next-coder-6c79f",
+  storageBucket: "next-coder-6c79f.appspot.com",
+  messagingSenderId: "532712299717",
+  appId: "1:532712299717:web:5d5e5841d477e73d2464ac",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
